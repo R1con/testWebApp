45f5c2143bf0deedff9b3db589b7f17d55e172b2
R1con <52736169+R1con@users.noreply.github.com>
2023-06-19 09:39:10 +0300

    webapp

--- src/main/java/com/reliab/TestWebApp/properties/BotProperties.java
+++ src/main/java/com/reliab/TestWebApp/properties/BotProperties.java
@@ -1,19 +1,16 @@
 package com.reliab.TestWebApp.properties;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Setter
+@Getter
 @ConfigurationProperties(prefix = "bot")
-@Data
 public class BotProperties {
-    @Value("${bot.name}")
     private String name;
-    @Value("${bot.token}")
     private String token;
-    @Value("${bot.url}")
     private String url;
-
 }
